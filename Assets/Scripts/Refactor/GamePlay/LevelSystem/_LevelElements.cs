3b84e1650643da8dfe3e35b600f9b5ca3e804cd9
TuanDat <letuandat556@gmail.com>
2024-04-16 11:36:17 +0700

    Debug click play event

--- Assets/Scripts/Refactor/GamePlay/LevelSystem/_LevelElements.cs
+++ Assets/Scripts/Refactor/GamePlay/LevelSystem/_LevelElements.cs
@@ -1,7 +1,21 @@
+using TMPro;
 using UnityEngine;
 
 namespace Core.GamePlay.LevelSystem{
-    public class _LevelSystem: MonoBehaviour{
-        
+    public class _LevelElements: MonoBehaviour{
+        private const string _levelTextFormat = "LEVEL ";
+
+        [Header("Elements")]
+        [SerializeField] private TMP_Text _levelText;
+        private int _currentLevel;
+
+        public void SetLevel(int level){
+            _currentLevel = level;
+            _levelText.text = _levelTextFormat + _currentLevel;
+        }
+
+        public void OnClickLevelPlay(){
+            Debug.Log("Play Level: " + _currentLevel);
+        }
     }
 }
