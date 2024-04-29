f7ac3c02f01f4c35937e57ba9dd949de4996b7c4
TuanDat <letuandat556@gmail.com>
2024-04-29 14:13:29 +0700

    add firebase analytics

--- Assets/Scripts/Refactor/Extensions/new-starter-kit-main-Assets-MyTools/Assets/MyTools/Analysis/Firebase/FirebaseManager.cs
+++ Assets/Scripts/Refactor/Extensions/new-starter-kit-main-Assets-MyTools/Assets/MyTools/Analysis/Firebase/FirebaseManager.cs
@@ -1,58 +1,59 @@
-﻿// using Firebase;
-// //using Firebase.Analytics;
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using Firebase.Extensions;
-// using Firebase.RemoteConfig;
-// using UnityEngine;
+﻿using Firebase;
+//using Firebase.Analytics;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Firebase.Extensions;
+using Firebase.RemoteConfig;
+using UnityEngine;
+using Firebase.Analytics;
 
-// public class FirebaseManager : SingletonMonoBehaviour<FirebaseManager>
-// {
-//    private const string TAG = "[FIREBASE]";
-//    public FirebaseApp app;   
-//    public bool isOk = false;
+public class FirebaseManager : SingletonMonoBehaviour<FirebaseManager>
+{
+   private const string TAG = "[FIREBASE]";
+   public FirebaseApp app;   
+   public bool isOk = false;
 
-//    public override void Awake()
-//    {
-//        Init();
-//        GlobalEventManager.Instance.EvtSendEvent += SendEvent;
-//      //  GlobalEventManager.Instance.EvtUpdateUserProperties += SetUserPropeties;
-//    }
+   public override void Awake()
+   {
+       Init();
+       GlobalEventManager.Instance.EvtSendEvent += SendEvent;
+     //  GlobalEventManager.Instance.EvtUpdateUserProperties += SetUserPropeties;
+   }
 
-//    private void OnDestroy()
-//    {
-//        if (GlobalEventManager.Exists())
-//        {
-//            GlobalEventManager.Instance.EvtSendEvent -= SendEvent;
-//          //  GlobalEventManager.Instance.EvtUpdateUserProperties -= SetUserPropeties;
-//        }
-//    }
+   private void OnDestroy()
+   {
+       if (GlobalEventManager.Exists())
+       {
+           GlobalEventManager.Instance.EvtSendEvent -= SendEvent;
+         //  GlobalEventManager.Instance.EvtUpdateUserProperties -= SetUserPropeties;
+       }
+   }
 
-//    public void Init()
-//    {
-//       Debug.Log($"{TAG} Init...");
-//        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
-//        {
-//            var dependencyStatus = task.Result;
-//            if (dependencyStatus == Firebase.DependencyStatus.Available)
-//            {
-//                app = FirebaseApp.DefaultInstance;
-//                isOk = true;
-//                FetchValue();
-//                FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+   public void Init()
+   {
+      Debug.Log($"{TAG} Init...");
+       FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+       {
+           var dependencyStatus = task.Result;
+           if (dependencyStatus == Firebase.DependencyStatus.Available)
+           {
+               app = FirebaseApp.DefaultInstance;
+               isOk = true;
+               FetchValue();
+               FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
 
-//                // Notifacation
-//                //Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
-//                //Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
-//                Debug.Log($"{TAG} Init done!");
-//            }
-//            else
-//            {
-//               Debug.Log($"{TAG} Init fail!");
-//            }
-//        });
-//    }
+               // Notifacation
+               //Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+               //Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+               Debug.Log($"{TAG} Init done!");
+           }
+           else
+           {
+              Debug.Log($"{TAG} Init fail!");
+           }
+       });
+   }
 
 //    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
 //    {
@@ -63,44 +64,44 @@
 //    {
 //        Debug.Log("Received a new message from: " + e.Message.From);
 //    }
-//    private void SendEvent(string eName, Parameter[] parameters)
-//    {
-//        if (!isOk)
-//            return;
-//        if (parameters == null)
-//        {
-//            FirebaseAnalytics.LogEvent(eName);
-//            //Debug.LogError($"{eName}");
-//        }
-//        else
-//        {
-//            FirebaseAnalytics.LogEvent(eName, parameters);
-//          //  Debug.LogError($"{eName}_{parameters}");
-//        }
-//    }
-//    private void SetUserPropeties()
-//    {
-//        if (!isOk) return;
-//        //FirebaseAnalytics.LogEvent(eName);
-//        //Debug.LogError(eName);
-//    }
+   private void SendEvent(string eName, Parameter[] parameters)
+   {
+       if (!isOk)
+           return;
+       if (parameters == null)
+       {
+           FirebaseAnalytics.LogEvent(eName);
+           //Debug.LogError($"{eName}");
+       }
+       else
+       {
+           FirebaseAnalytics.LogEvent(eName, parameters);
+         //  Debug.LogError($"{eName}_{parameters}");
+       }
+   }
+   private void SetUserPropeties()
+   {
+       if (!isOk) return;
+       //FirebaseAnalytics.LogEvent(eName);
+       //Debug.LogError(eName);
+   }
 
-//    private void FetchValue()
-//    {
-//        SetDefaultValue();
-//        TimeSpan time = new TimeSpan(0, 0, 10);
-//        FirebaseRemoteConfig.DefaultInstance.FetchAsync(time).ContinueWithOnMainThread(task =>
-//        {
-//            var info = FirebaseRemoteConfig.DefaultInstance.Info;
-//            if (info.LastFetchStatus == LastFetchStatus.Success)
-//            {
-//               Debug.Log($"{TAG}: Read remote config...");
-//            }
-//        });
-//    }
+   private void FetchValue()
+   {
+       SetDefaultValue();
+       TimeSpan time = new TimeSpan(0, 0, 10);
+       FirebaseRemoteConfig.DefaultInstance.FetchAsync(time).ContinueWithOnMainThread(task =>
+       {
+           var info = FirebaseRemoteConfig.DefaultInstance.Info;
+           if (info.LastFetchStatus == LastFetchStatus.Success)
+           {
+              Debug.Log($"{TAG}: Read remote config...");
+           }
+       });
+   }
 
-//    private void SetDefaultValue()
-//    {
+   private void SetDefaultValue()
+   {
         
-//    }
-// }
+   }
+}
